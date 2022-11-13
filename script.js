6bacd36828b9640136648ea4962fbee8b810e9c6
fatih-celebi <celebifatih.c1@gmail.com>
2022-11-13 13:58:06 +0000

    Complete Task 100

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  console.log(allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+
+  for(let i = 0 ; i < episodeList.length ; i ++ ){
+    const body = document.getElementById("container")
+    const dive = document.createElement("div")
+    const pHeader = document.createElement("p");
+    const img = document.createElement("img");
+    const pSum = document.createElement("p");
+    img.src = episodeList[i].image.medium; // display medium Img
+    pHeader.innerText = `${episodeList[i].name} - S01E0${i + 1} `; // display heder and number of episod
+    pHeader.id="header-episode";
+    pSum.innerHTML = episodeList[i].summary; // display summary
+    body.appendChild(dive)
+    dive.appendChild(pHeader);
+    dive.appendChild(img);
+    dive.appendChild(pSum);
+  }}
 
 window.onload = setup;
