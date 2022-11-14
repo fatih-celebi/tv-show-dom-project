8a80eb166689687af5ba13848173bb7013376ee7
fatih-celebi <celebifatih.c1@gmail.com>
2022-11-14 23:25:15 +0000

    Update Task 350

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const searchInput = document.querySelector(".search");
 
 function setup() {
-  let url = "https://api.tvmaze.com/shows/82/episodesaa";
+  let url = "https://api.tvmaze.com/shows/82/episodes";
   fetch(url).then(rep=> rep.json()).then(data=> makePageForEpisodes(data)).catch(err=>displayError(err));
 
     function displayError(err) {
-    let errorText = document.getElementById("error-text");
-    console.log(errorText);
+    let errorText = document.createElement("p");
+    let errorPage = document.getElementById("error-text");
+    errorText.innerText = "Sorry, this page couldn`t load.";
+    errorPage.appendChild(errorText);
     }
 }
 
